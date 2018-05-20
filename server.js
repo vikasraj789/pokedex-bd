ae14f6c8a17999ef2765de39b61f3c7dce388a33
Vikas Reddy <vikasreddy@Sahils-MacBook-Pro.local>
2018-05-20 09:15:23 +0530

    Added body parser

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require("express");
+const express = require( "express" );
+const bodyParser = require( "body-parser" );
+
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-app.get("/", (req, res) => {
-    res.send({ hello: "world" });
-});
-app.listen(PORT);
-console.log(`Your server is running on port -- ${PORT}`);
+// middlewares
+app.use( bodyParser.json() );
+app.use( ( req, res, next ) => {
+    res.header( "Access-Control-Allow-Origin", "*" );
+    next();
+} );
+
+// Rest api's
+app.get( "/", ( req, res ) => {
+    res.send( { "hello": "world" } );
+} );
+
+console.log( `Your server is running on port -- ${PORT}` );
+app.listen( PORT );
