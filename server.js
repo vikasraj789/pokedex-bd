22f137455f34150780b5ae0ad3094c00d62741ea
Vikas Reddy <vikasreddy@Sahils-MacBook-Pro.local>
2018-05-20 09:40:37 +0530

    Added pokedex promise package

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const express = require( "express" );
-const bodyParser = require( "body-parser" );
+const express = require("express");
+const bodyParser = require("body-parser");
+const Pokedex = require("pokedex-promise-v2");
 
 const app = express();
+const options = {
+    protocol: "https",
+    timeout: 5 * 1000 // 5s
+};
+const P = new Pokedex(options);
 const PORT = process.env.PORT || 3030;
 
 // middlewares
-app.use( bodyParser.json() );
-app.use( ( req, res, next ) => {
-    res.header( "Access-Control-Allow-Origin", "*" );
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
     next();
-} );
+});
 
 // Rest api's
-app.get( "/", ( req, res ) => {
-    res.send( { "hello": "world" } );
-} );
+app.get("/", async (req, res) => {
+    const res = await P.getPokedexsList();
+    const data = await res.json();
+    res.send(data);
+});
 
-console.log( `Your server is running on port -- ${PORT}` );
-app.listen( PORT );
+console.log(`Your server is running on port -- ${PORT}`);
+app.listen(PORT);
