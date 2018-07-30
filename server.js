4e1656e45238279f44c078652c527dcc9bdb852c
Mac 3 <mac3@MacBook-Pro-39.local>
2018-07-30 11:33:58 +0800

    Revert "Added dummy data"
    This reverts commit 6ffdd40f094bee4a2b08f624105f036c229b45c9.
    
    Revert

--- server.js
+++ server.js
@@ -1,30 +1,15 @@
 const express = require("express");
-const https = require("https");
-const fs = require("fs");
 const bodyParser = require("body-parser");
 const Pokedex = require("pokedex-promise-v2");
 const MongoClient = require("mongodb").MongoClient;
-const dummyData = [];
-
-for (let i = 0; i < 100; i++) {
-    const temp = {
-        avatar: "http://pokeapi.co/media/sprites/pokemon/back/female/12.png",
-        types: ["grass", "football", "sport"],
-        name: `balbusaur${i}`,
-        isFav: false
-    };
-    dummyData.push(temp);
-}
 
 const app = express();
 const options = {
     protocol: "https",
-    timeout: 10 * 1000 // 5s
+    timeout: 5 * 1000 // 5s
 };
-
 // Mongo url
-const mongoUrl =
-    "mongodb://vikasmLab:9440926435@ds237748.mlab.com:37748/pokedex";
+const mongoUrl = "mongodb://vikasmLab:9440926435@ds237748.mlab.com:37748/pokedex";
 const P = new Pokedex(options);
 const PORT = process.env.PORT || 3030;
 // Database Name
@@ -37,10 +22,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "*");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
@@ -51,25 +33,17 @@ app.put("/favourite/:userId", async (req, res) => {
         const userId = req.param("userId");
         let client = await MongoClient.connect(mongoUrl);
         const db = client.db(dbName);
-        const user = await db
-            .collection("favourites")
-            .findOne({ userId: userId });
+        const user = await db.collection("favourites").findOne({ userId: userId });
         let update, insert;
         if (user) {
-            update = await db
-                .collection("favourites")
-                .update({ userId: userId }, { $addToSet: { names: name } });
+            update = await db.collection("favourites").update({ userId: userId }, { $addToSet: { names: name } });
         } else {
-            insert = await db
-                .collection("favourites")
-                .insert({ userId: userId, names: [name] });
+            insert = await db.collection("favourites").insert({ userId: userId, names: [name] });
         }
         client.close();
         if (update || insert) {
-            console.log("Created");
             res.status(200).send({ status: 200, message: "Created" });
         } else {
-            console.log("Bad Request");
             res.status(400).send({ status: 400, message: "Bad Request" });
         }
     } catch (exc) {
@@ -84,15 +58,11 @@ app.put("/unFavourite/:userId", async (req, res) => {
         const userId = req.param("userId");
         let client = await MongoClient.connect(mongoUrl);
         const db = client.db(dbName);
-        const update = await db
-            .collection("favourites")
-            .update({ userId: userId }, { $pull: { names: name } });
+        const update = await db.collection("favourites").update({ userId: userId }, { $pull: { names: name } });
         client.close();
         if (update) {
-            console.log("Updated");
             res.status(200).send({ status: 200, message: "Updated" });
         } else {
-            console.log("Bad Request");
             res.status(400).send({ status: 400, message: "Bad Request" });
         }
     } catch (exc) {
@@ -101,63 +71,39 @@ app.put("/unFavourite/:userId", async (req, res) => {
     }
 });
 
-// app.post("/", async (req, res) => {
-//     try {
-//         const { userId } = req.body;
-//         const resp = await P.getPokemonsList(req.body.params);
-//         if (resp && resp.results) {
-//             const data = [];
-//             let client = await MongoClient.connect(mongoUrl);
-//             const db = client.db(dbName);
-//             const doc = await db
-//                 .collection("favourites")
-//                 .findOne({ userId: userId });
-//             client.close();
-//             const favs = doc && doc.names ? doc.names : [];
-//             for (let poke of resp.results) {
-//                 const rawData = await P.getPokemonByName(poke.name);
-//                 const types = [];
-//                 if (rawData && rawData.types) {
-//                     rawData.types.forEach((type) => {
-//                         types.push(type.type.name);
-//                     });
-//                 }
-//                 data.push({
-//                     avatar: rawData.sprites.front_default,
-//                     types: types,
-//                     name: poke.name,
-//                     isFav: favs.indexOf(poke.name) > -1
-//                 });
-//             }
-//             res.send(data);
-//         }
-//     } catch (exc) {
-//         console.log(exc);
-//         res.status(500).send({ status: 500, message: "Internal server error" });
-//     }
-// });
-
 app.post("/", async (req, res) => {
     try {
-        const { userId, params } = req.body;
-        res.send(dummyData.slice(params.offset, params.limit + params.offset));
+        const { userId } = req.body;
+        const resp = await P.getPokemonsList(req.body.params);
+        if (resp && resp.results) {
+            const data = [];
+            let client = await MongoClient.connect(mongoUrl);
+            const db = client.db(dbName);
+            const doc = await db.collection("favourites").findOne({ userId: userId });
+            client.close();
+            const favs = doc && doc.names ? doc.names : [];
+            for (let poke of resp.results) {
+                const rawData = await P.getPokemonByName(poke.name);
+                const types = [];
+                if (rawData && rawData.types) {
+                    rawData.types.forEach((type) => {
+                        types.push(type.type.name);
+                    });
+                }
+                data.push({
+                    avatar: rawData.sprites.front_default,
+                    types: types,
+                    name: poke.name,
+                    isFav: favs.indexOf(poke.name) > -1
+                });
+            }
+            res.send(data);
+        }
     } catch (exc) {
         console.log(exc);
         res.status(500).send({ status: 500, message: "Internal server error" });
     }
 });
-
-// https
-//     .createServer(
-//         {
-//             key: fs.readFileSync("./key.pem"),
-//             cert: fs.readFileSync("./cert.pem")
-//         },
-//         app
-//     )
-//     .listen(PORT, () => {
-//         console.log(`Your server is running on port -- ${PORT}`);
-//     });
 
 console.log(`Your server is running on port -- ${PORT}`);
 app.listen(PORT);
